94a7d1598d9428c0b7a8e2b9462ad3f5c02ede24
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-11-09 18:50:27 +0200

    Exercise 12.11 completed

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -4,7 +4,6 @@ const redis = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  console.log("testi1");
   const count = await redis.getAsync("added_todos");
   console.log("count: " + count)
   res.json({
